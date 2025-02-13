271401911c6b972bb0c8d68d1473c5447e6a856d
Felipe Nicolas Aguila <faguila899@gmail.com>
2025-02-13 17:36:38 -0300

    migrations

--- store_backend/inventory/models.py
+++ store_backend/inventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Shelf(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField(default=0)
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
